ef3b464a716d58cb54374103bffb8548824255d2
jayant <jayantparmar420@gmail.com>
2022-03-12 23:18:34 +0530

    added welcome to the gambling world

---
+++ src/com/bridgelaqbz/Gambling_project.java
@@ -0,0 +1,9 @@
+package com.bridgelaqbz;
+
+public class Gambling_project {
+
+    public static void main(String[] args) {
+        System.out.println("welcome to the gambling world");
+        
+    }
+}
